be846a8175308f1f11ce9c2a547b80d10de73820
SergeyLubimov <logoskaizar@yandex.ru>
2021-12-10 15:15:36 +0600

    Лаб 3 готова

--- Programming_lab-01/Programming_lab-01/Programming_lab-01.cpp
+++ Programming_lab-01/Programming_lab-01/Programming_lab-01.cpp
@@ -9,7 +9,7 @@ using namespace std;
 int main()
 {
     Shop shopA;
-    Shop *shopB;
+    Shop* shopB;
 
     shopB = new Shop();
 
@@ -19,60 +19,62 @@ int main()
     shopA.setNameOfShop("Shop A");
     shopB->setNameOfShop("Shop B");
 
-    BatchOfGoods a1, a2, a3;
+    //BatchOfGoods a1, a2, a3;
 
-    BatchOfGoods *b1, *b2, *b3;
+    BatchOfGoods* a = new BatchOfGoods[3];
 
-    a1.setNameOfGoods("A");
-    a1.setIdOfGood(2256);
-    a1.setPriceOfGoods(150);
-    a1.setQuantityOfGoods(16);
-    a1.setDateOfBatch("11.01.1996");
+    BatchOfGoods* b1, * b2, * b3;
 
-    a2.setNameOfGoods("A");
-    a2.setIdOfGood(1200564);
-    a2.setPriceOfGoods(178);
-    a2.setQuantityOfGoods(70);
-    a2.setDateOfBatch("11.01.2008");
+    BatchOfGoods* b[3];
 
-    a3.setNameOfGoods("A");
-    a3.setIdOfGood(420751);
-    a3.setPriceOfGoods(73);
-    a3.setQuantityOfGoods(16);
-    a3.setDateOfBatch("01.11.2008");
+    for (int i = 0; i < 3; i++) 
+        b[i] = new BatchOfGoods();
 
-    b1 = new BatchOfGoods();
-    b1->setNameOfGoods("B");
-    b1->setIdOfGood(398755);
-    b1->setPriceOfGoods(15);
-    b1->setQuantityOfGoods(8);
-    b1->setDateOfBatch("07.02.2019");
+    a[0].setNameOfGoods("A");
+    a[0].setIdOfGood(2256);
+    a[0].setPriceOfGoods(150);
+    a[0].setQuantityOfGoods(16);
+    a[0].setDateOfBatch("11.01.1996");
 
+    a[1].setNameOfGoods("A");
+    a[1].setIdOfGood(1200564);
+    a[1].setPriceOfGoods(178);
+    a[1].setQuantityOfGoods(70);
+    a[1].setDateOfBatch("11.01.2008");
 
-    b2 = new BatchOfGoods();
-    b3 = new BatchOfGoods();
+    a[2].setNameOfGoods("A");
+    a[2].setIdOfGood(420751);
+    a[2].setPriceOfGoods(73);
+    a[2].setQuantityOfGoods(16);
+    a[2].setDateOfBatch("01.11.2008");
 
-    bool is_fullB2 = false;
-    bool is_fullB3 = false;
+    b[0]->setNameOfGoods("B");
+    b[0]->setIdOfGood(398755);
+    b[0]->setPriceOfGoods(15);
+    b[0]->setQuantityOfGoods(8);
+    b[0]->setDateOfBatch("07.02.2019");
 
-    is_fullB2 = b2->launchNewBatchForm();    
-    is_fullB3 = b3->launchNewBatchForm();
+    bool is_fullB[3];
 
-    shopA.addBatch(&a1);
-    shopA.addBatch(&a2);
-    shopA.addBatch(&a3);
+    is_fullB[0] = false;
 
-    shopB->addBatch(a1.cloneBarch());
-    shopB->addBatch(a2.cloneBarch());
-    shopB->addBatch(a3.cloneBarch());
+    for (int i = 1; i < 3; i++)
+    {
+        is_fullB[i] = false;
+        is_fullB[i] = b[i]->launchNewBatchForm();
+    }
 
-    shopA.addBatch(b1);
-    if (is_fullB2) shopA.addBatch(b2);
-    if (is_fullB3) shopA.addBatch(b3);
+    for (int i = 0; i < 3; i++)
+    {
+        shopA.addBatch(&a[i]);
+        shopB->addBatch(a[i].cloneBarch());
 
-    shopB->addBatch(b1->cloneBarch());
-    if (is_fullB2) shopB->addBatch(b2->cloneBarch());
-    if (is_fullB3) shopB->addBatch(b3->cloneBarch());
+        if (is_fullB[i])
+        {
+            shopA.addBatch(b[i]);
+            shopB->addBatch(b[i]->cloneBarch());
+        }
+    }
 
     shopA.displayAssortment();
     shopB->displayAssortment(); 
